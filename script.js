8b1b2dc8762a4d713d356adf63c979c79fd27bd9
juanpabloruiz <pabloruiz1980@gmail.com>
2024-05-28 19:46:45 -0300

    Countdown changed for days

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("progressBar").style.width = progress + "%";
   }
 
-  // Función para calcular y mostrar el contador de cuenta regresiva en segundos
+  // Función para calcular y mostrar el contador de cuenta regresiva en días
   function updateCountdown() {
     // Fecha de nacimiento
     var birthDate = new Date("1980-06-19");
@@ -38,17 +38,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // Diferencia en milisegundos
     var difference = futureDate - currentDate;
     
-    // Convertir milisegundos a segundos
-    var totalSeconds = Math.floor(difference / 1000);
+    // Convertir milisegundos a días
+    var totalDays = Math.floor(difference / (1000 * 60 * 60 * 24));
 
-    // Formatear los segundos con puntos
-    var formattedSeconds = totalSeconds.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    // Formatear los días con puntos
+    var formattedDays = totalDays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-    // Mostrar el contador de cuenta regresiva solo en segundos
-    document.getElementById("countdown").innerHTML = "<h5>Me quedan:</h5><h4>" + formattedSeconds + " segundos.</h4>";
+    // Mostrar el contador de cuenta regresiva solo en días
+    document.getElementById("countdown").innerHTML = "<h5>Me quedan:</h5><h4>" + formattedDays + " días.</h4>";
   }
-
-  
 
   // Actualizar el progreso y el contador cada segundo
   setInterval(function() {
@@ -59,5 +57,4 @@ document.addEventListener("DOMContentLoaded", function() {
   // Actualizar la barra de progreso y el contador al cargar la página
   updateProgressBar();
   updateCountdown();
-
 });
